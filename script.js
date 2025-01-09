bd6a21c7e2ea7746b3e32488db5a728f668cdcd1
turtle-txt <sreelalnet@gmail.com>
2025-01-09 20:54:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,6 +35,25 @@ window.addEventListener('load', function () {
         setTimeout(() => {
             section.style.opacity = 1;
             section.style.transition = "opacity 1s ease";
-        }, index * 500); // Staggered fade-in effect
+        }, index * 500);
     });
 });
+function openProject(evt, projectName) {
+
+    let tabcontent = document.getElementsByClassName("tabcontent");
+    for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+
+    let tablinks = document.getElementsByClassName("tablinks");
+    for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+
+  
+    document.getElementById(projectName).style.display = "block";
+    evt.currentTarget.classList.add("active");
+}
+
+document.getElementsByClassName("tablinks")[0].click();
